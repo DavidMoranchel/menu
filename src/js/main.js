9945dd28562e2f64fd01ee9765ec18f5d4a410bc
David Moranchel <david.arcshtag@gmail.com>
2017-04-23 15:57:05 -0500

    complete animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 $('.btn-menu').click(function () {
-	if ($('.btn-menu').hasClass('active-btn-menu')) {
-		$(this).removeClass('active-btn-menu');
+	if ($('.btn-sub-menu').hasClass('active-sub-menu')) {
+		$('.btn-sub-menu').removeClass('active-sub-menu')
+		$('.btn-menu').removeClass('active-menu');
 	}else{
-		$(this).addClass('active-btn-menu');
+		$('.btn-sub-menu').addClass('active-sub-menu')
+		$('.btn-menu').addClass('active-menu');
 	}
 });
+// $(".btn-menu").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+// 	console.log('acaboooo');
+// });
